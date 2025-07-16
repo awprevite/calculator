668ec6e09379e94847a8d9f6dd09a769ce1f9ca3
awprevite <awprevite@gmail.com>
2025-07-16 10:56:16 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center items-center w-screen h-screen'>
+        <div className='flex flex-col justify-center items-center rounded-lg p-4 bg-blue-500/30'>
+          <h1 className='text-3xl text-center'>Initial</h1>
+          <h1 className='text-3xl text-center'>Commit</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
